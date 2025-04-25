cffa7a62cd02bb67eb7be85a2b6447bec30f5084
MaximusXVW <MAX2161324@maricopa.edu>
2025-04-24 21:51:31 -0700

    Sorting Driver

--- src/main/java/com/example/sorting/SortingUtility.java
+++ src/main/java/com/example/sorting/SortingUtility.java
@@ -4,16 +4,53 @@ public class SortingUtility {
 
 
     public static <T extends Comparable<T>> void gnomeSort(T[] data) {
+        int index = 0;
+        while(index == 0) {
+            if (index == 0) {
+                index++;
+            } else if (data[index].compareTo(data[index - 1]) >= 0) {
+                index++;
+            } else {
+                swap(data, index, index - 1);
+                index--;
+            }
+        }
 
         // TODO implement Gnome Sort here
     }
 
 
     public static <T extends Comparable<T>> void cocktailShakerSort(T[] data) {
+        boolean swapped = true;
+        int start = 0;
+        int end = data.length - 1;
 
-        // TODO implement Cocktail Shaker Sort here
+        while (swapped) {
+            swapped = false;
+            for (int i = start; i < end; i++) {
+                if (data[i].compareTo(data[i + 1]) > 0) {
+                    swap(data, i, i + 1);
+                    swapped = true;
+                }
+            }
+            end--;
+            if (swapped) {
+                swapped = false;
+
+                // Otherwise, reset the swapped flag so that it can be used in the next stage
+
+
+                for (int i = end - 1; i >= start; i--) {
+                    if (data[i].compareTo(data[i + 1]) > 0) {
+                        swap(data, i, i + 1);
+                        swapped = true;
+                    }
+                }
+                start++;
+            }
+            // TODO implement Cocktail Shaker Sort here
+        }
     }
-
 
     public static <T extends Comparable<T>> void shellSort(T[] data) {
 
